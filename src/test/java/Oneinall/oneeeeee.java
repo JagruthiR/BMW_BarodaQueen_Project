683ef38f6ef922857f11eaf0310a814f16b949c9
Charvik <Charvik@LAPTOP-S0IS0QVR>
2024-09-17 11:50:08 +0530

    222 ndnd dn

---
+++ src/test/java/Oneinall/oneeeeee.java
@@ -0,0 +1,19 @@
+package Oneinall;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class oneeeeee {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
